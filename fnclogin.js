ef9202eed7b87d2cb69248af055509efac0cebd8
ThanaphatS-kim <kimthanaphat180248@gmail.com>
2024-11-04 23:15:43 +0700

    Add files via upload
    ggbro

---
+++ fnclogin.js
@@ -0,0 +1,9 @@
+const formOpenBtn = document.querySelector("#form-open");
+home = document.querySelector(".home");
+formContainer = document.querySelector(".form_container");
+formClose = document.querySelector(".form_close");
+singupBtn = document.querySelector("#signup");
+loginBtn = document.querySelector("#login")
+
+formOpenBtn.addEventListener("click", () => home.classList.add("show"))
+formClose.addEventListener("click", () => home.classList.remove("show"))
